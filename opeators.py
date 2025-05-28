54b80f257cc5108a59244979df14ef75743d89f3
NannaVishal <162903898+NannaVishal@users.noreply.github.com>
2025-05-28 22:39:42 +0530

    Add files via upload

---
+++ opeators.py
@@ -0,0 +1,33 @@
+#simple interest
+p=35
+r=57
+s=98
+si=(p*r*s)/100
+print("simple interest:",si)
+
+# tempertaure
+c=259
+f=(c*9/5)+32
+print("Fahrenheit:" ,f)
+
+#Average of three numbers
+a= 900
+b=678
+c=45678
+average =(a+b+c)/3
+print("Average:",average)
+
+#area of circle
+pi=32
+r=3
+a=pi*r**2
+print("Area of circle:",a)
+
+#swap
+a=78
+b=98
+x=b
+b=a
+a=x
+print(a)
+print(b)
